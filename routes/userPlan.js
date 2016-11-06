c788638f87973e284b73f1ea829dfd3543892a74
rakesh <rak1994kumar@gmail.com>
2016-11-06 20:53:57 +0530

    added dependency

---
+++ routes/userPlan.js
@@ -0,0 +1,14 @@
+
+var mongoose = require('mongoose');
+var planModel = require('../models/plan.js');
+var linkModel = require('../models/link.js');
+var q = require('q');
+var queryResolver = require('../lib/queryResolver.js');
+require('mongoose-query-paginate');
+
+var plans = {
+
+
+}
+
+module.exports = plans;
