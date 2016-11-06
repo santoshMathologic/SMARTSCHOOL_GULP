6a5cc771e5bcc0bc270da386b2bffb72ae0cfd24
rakesh <rak1994kumar@gmail.com>
2016-11-07 00:34:38 +0530

    userPlan working

--- public/ng/utils/serverFetch.js
+++
@@ -1,8 +0,0 @@
-String.prototype.isEmpty = function() {
-    return (this.length === 0 || !this.trim());
-};
-
-String.prototype.capitalizeFirstLetter = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1) + this.charAt(4).toUpperCase();
-    //return this.toUpperCase();
-}
